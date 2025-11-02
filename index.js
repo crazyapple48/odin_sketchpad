21d96a2cd42247672c497b7dd34dcdc571a3a3df
Kevin Penner <111170452+crazyapple48@users.noreply.github.com>
2025-11-01 17:38:58 -0700

    Add javascript grid of divs

---
+++ index.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container");
+
+for (let i = 0; i < (16*16); i++) {
+    const itemWidthPercentage = 100 / 16;
+
+    const box = document.createElement("div");
+    box.className = "box";
+    
+    box.style.cssText = `
+        flex-basis: ${itemWidthPercentage}%;
+        height: ${itemWidthPercentage}%;
+    `
+
+    container.appendChild(box);
+}
