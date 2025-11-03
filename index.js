9ed888c6064408c99f44c823c3f204fa0d5e8986
Kevin Penner <111170452+crazyapple48@users.noreply.github.com>
2025-11-03 07:29:31 -0700

    Add button to reset grid with custom user values

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("#btn");
 
-for (let i = 0; i < (16*16); i++) {
-    const itemWidthPercentage = 100 / 16;
+function createGrid(size) {
+    container.innerHTML = "";
 
-    const box = document.createElement("div");
-    box.className = "box";
-    
-    box.style.cssText = `
-        flex-basis: ${itemWidthPercentage}%;
-        height: ${itemWidthPercentage}%;
-    `
+	for (let i = 0; i < size * size; i++) {
+		const itemWidthPercentage = 100 / size;
 
-    box.addEventListener("mouseenter", () => {
-        box.classList.add("active");
-    })
+		const box = document.createElement("div");
+		box.className = "box";
 
-    container.appendChild(box);
+		box.style.cssText = `
+            flex-basis: ${itemWidthPercentage}%;
+            height: ${itemWidthPercentage}%;
+            `;
+
+		box.addEventListener("mouseenter", () => {
+			box.classList.add("active");
+		});
+
+		container.appendChild(box);
+	}
 }
+
+function getSize() {
+    let input = prompt("How many squares do you want on each side? (0-100)");
+
+    let isValid = false;
+
+    while (!isValid) {
+        if (input > 100 || input <= 0) {
+            input = prompt("Enter a number between 0 and 100");
+        } else {
+            isValid = true;
+        }
+    }
+
+    createGrid(parseInt(input));
+
+    return parseInt(input);
+}
+
+const userChoice = getSize();
+
+button.addEventListener("click", () => getSize());
